c7113fdec168e580f07ca4c7fb83dfab708057cc
chiara.calabrese90@gmail.com <chiara.calabrese90@gmail.com@75f80eb4-fa17-98f1-3056-120358668a0f>
2014-04-18 20:28:26 +0000

    riportare le activity in actionbaractivity

--- StayMilano/src/com/staymilano/SplashActivity.java
+++ StayMilano/src/com/staymilano/SplashActivity.java
@@ -1,16 +1,9 @@
 package com.staymilano;
 
-import android.support.v4.app.Fragment;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
